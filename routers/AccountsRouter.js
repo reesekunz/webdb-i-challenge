be9d2122daa06c2640ea045799f974807a3013df
Reese Kunz <reesekunz21@gmail.com>
2019-09-09 13:31:02 -0700

    added middleware for validation

--- routers/AccountsRouter.js
+++ routers/AccountsRouter.js
@@ -19,7 +19,7 @@ router.get("/", (request, response) => {
     });
 });
 
-router.get("/:id", (request, response) => {
+router.get("/:id", accountIdValidation, (request, response) => {
   // in SQL the translation is select * from accounts where id = 1
   const { id } = request.params;
   db("accounts")
@@ -35,7 +35,7 @@ router.get("/:id", (request, response) => {
     });
 });
 
-router.post("/", (request, response) => {
+router.post("/", accountValidation, (request, response) => {
   // in SQL the translation is insert into accounts () values ()
   const accountData = request.body;
   // be sure to validate the postData before inserting into the db (using custom middleware)
@@ -49,22 +49,27 @@ router.post("/", (request, response) => {
     });
 });
 
-router.put("/:id", (request, response) => {
-  // in SQL the translation is update account set ... where id = 123
-  const changes = request.body;
-  const { id } = request.params;
-  db("accounts")
-    .where({ id })
-    .update(changes)
-    .then(count => {
-      response.status(200).json({ message: `updated ${count} account` });
-    })
-    .catch(error => {
-      response.json(error);
-    });
-});
+router.put(
+  "/:id",
+  accountIdValidation,
+  accountValidation,
+  (request, response) => {
+    // in SQL the translation is update account set ... where id = 123
+    const changes = request.body;
+    const { id } = request.params;
+    db("accounts")
+      .where({ id })
+      .update(changes)
+      .then(count => {
+        response.status(200).json({ message: `updated ${count} account` });
+      })
+      .catch(error => {
+        response.json(error);
+      });
+  }
+);
 
-router.delete("/:id", (request, response) => {
+router.delete("/:id", accountIdValidation, (request, response) => {
   const { id } = request.params;
   // in SQL the translation is delete from accounts where ...
   db("accounts")
@@ -77,5 +82,38 @@ router.delete("/:id", (request, response) => {
       response.json(error);
     });
 });
+
+// custom middleware for requirements validation
+
+// name and budget required
+function accountValidation(request, response, next) {
+  if (!request.body) {
+    response.status(400).json({ message: "account data not found" });
+  } else if (!request.body.name) {
+    response.status(400).json({ message: "name is required" });
+  } else if (!request.body.budget) {
+    response.status(400).json({ message: "budget is required" });
+  }
+  next();
+}
+
+function accountIdValidation(request, response, next) {
+  const { id } = request.params;
+  db.get(id)
+    .then(accountID => {
+      console.log("account id validation success", accountID);
+      if (accountID) {
+        next();
+      } else {
+        response.status(400).json({ message: "account id not found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      response
+        .status(500)
+        .json({ message: "failed account validation request" });
+    });
+}
 
 module.exports = router;
