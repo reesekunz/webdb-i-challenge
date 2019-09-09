9ac3f001eedabebc7bf960f0c43c8565d641c465
Reese Kunz <reesekunz21@gmail.com>
2019-09-09 12:54:04 -0700

    added accounts router to server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
+const AccountsRouter = require("./routers/AccountsRouter");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", AccountsRouter);
 
 module.exports = server;
